63c7d9be3586fe6186e565bc31c6b2f19b24604d
Vir Sanghavi <67278851+VirSanghavi@users.noreply.github.com>
2026-09-20 09:11:30 -0700

    infra: the control plane image pins follow the v1.5.4 tag (#505)
    Signed-off-by: Vir Sanghavi <67278851+VirSanghavi@users.noreply.github.com>

--- infra/terraform/modules/control-plane/variables.tf
+++ infra/terraform/modules/control-plane/variables.tf
@@ -154,7 +154,7 @@ variable "image_repository" {
 }
 variable "image_tag" {
   type    = string
-  default = "v1.5.3"
+  default = "v1.5.4"
 }
 variable "image_digest" {
   type        = string
--- infra/terraform/stacks/control-plane/variables.tf
+++ infra/terraform/stacks/control-plane/variables.tf
@@ -89,7 +89,7 @@ variable "image_repository" {
 
 variable "image_tag" {
   type    = string
-  default = "v1.5.3"
+  default = "v1.5.4"
 }
 
 variable "image_digest" {
